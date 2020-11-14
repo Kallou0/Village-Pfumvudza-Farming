115985174119432a2395c7d6f2c436edd321e599
Marshal Nzenza <mnzenza@zimswitch.co.zw>
2020-11-14 15:51:14 +0200

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div class="banner">
+		<div class="container">
+			<div class="top-nav">
+					<nav class="navbar navbar-default">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+					
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li><a class="active" href="index.html">Home</a></li>
+								<li><a href="#about" class="scroll">About</a></li>
+								<li><a href="#services" class="scroll">Services</a></li>
+								<li><a href="#works" class="scroll">Works</a></li>	
+								<li><a href="#team" class="scroll">Team</a></li>
+								<li><a href="#contact" class="scroll">Contact</a></li>
+							</ul>
+							<div class="clearfix"> </div>							
+						</div>	
+					</nav>		
+			</div>
+			<div class="w3layouts-logo">
+				<h1><a href="index.html">Agri <span>The Quality Products</span></a></h1>
+			</div>
+			<div class="w3-info">
+				<div class="w3l-mail">
+					<p><i class="fa fa-envelope-o" aria-hidden="true"></i><a href="mailto:info@example.com">info@example.com</a></p>
+				</div>
+				<div class="w3ls-phone">
+					<p><i class="fa fa-phone" aria-hidden="true"></i>+263785890999</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
     </div>
   );
 }
