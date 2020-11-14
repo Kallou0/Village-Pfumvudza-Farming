43c5eaf12d1d548b18c0e3fe461c420cd7545e3b
Marshal Nzenza <mnzenza@zimswitch.co.zw>
2020-11-14 16:49:48 +0200

    styling of banner/nav bar done

---
+++ src/About.js
@@ -0,0 +1,68 @@
+function About() {
+    return(
+        <div class="about" id="about">
+		<div class="container">
+			<div class="agileits-about-grids">
+				<div class="col-md-4 agileinfo-about-left">
+					<div class="about-left-img">
+						
+					</div>
+				</div>
+				<div class="col-md-8 wthree-about-right">
+					<div class="about-right-info">
+						<h2>About Us</h2>
+						<div class="border"> </div>
+					</div>
+					<div class="about-slider">
+						<div class="slider">
+							<div class="callbacks_container">
+								<ul class="rslides" id="slider4">
+									<li>
+										<div class="w3ls-text">
+											<p>Donec feugiat tellus sem, laoreet iaculis orci lobortis vel. Sed sed luctus orci, at lacinia risus. Ut porttitor ante ac tincidunt elementum. Curabitur ex dolor, condimentum vitae nunc vel, pulvinar semper justo. Vestibulum et aliquet magna, maximus dapibus enim. Vestibulum ex lectus, posuere eu viverra at, mattis et enim. Nam efficitur sem et lectus fringilla, at pharetra nulla luctus. Nunc cursus, augue ac ultricies volutpat, neque erat congue justo, ac pretium tellus eros a neque. Integer ipsum sem, consectetur a mollis ac, cursus eu ipsum. Maecenas in diam tincidunt, ornare urna a, consequat ante. Cras eget maximus mi.</p>
+										</div>
+									</li>
+									<li>
+										<div class="w3ls-text">
+											<p>Nullam maximus dolor sed velit gravida, quis vestibulum eros sagittis. Nulla ultricies metus a turpis pretium volutpat. Cras consectetur tempus lectus id accumsan. Vivamus gravida justo mattis ex pretium, eu sollicitudin tortor ullamcorper. Quisque vitae diam molestie, tincidunt velit vitae, viverra nisl. Mauris ultrices commodo imperdiet. In urna odio, semper nec est non, pulvinar molestie quam. Etiam egestas varius nunc et rutrum. Curabitur tempor lacinia pharetra. Ut laoreet urna sed risus consequat laoreet. In volutpat sollicitudin volutpat. Praesent ut ex urna. Nunc vulputate tortor id egestas dignissim. </p>
+										</div>
+									</li>
+									<li>
+										<div class="w3ls-text">
+											<p>Ut porttitor, ante at tincidunt eleifend, quam massa laoreet odio, eu tincidunt lorem mauris ut velit. Etiam tempor nibh ut odio pellentesque, lacinia placerat dui hendrerit. Integer mi lorem, consequat non malesuada et, pellentesque eget nisl. Praesent sed dui ac lectus vehicula molestie. Etiam maximus ultrices dolor, sit amet tincidunt purus posuere in. Donec tempus sit amet erat non bibendum. Mauris semper ipsum elementum luctus hendrerit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus sit amet velit feugiat, tempor ante in, rutrum neque. Phasellus lacinia cursus leo.</p>
+										</div>
+									</li>
+								</ul>
+							</div>
+							
+								
+								{/* $(function () {
+								 
+								  $("#slider4").responsiveSlides({
+									auto: true,
+									pager:false,
+									nav:true,
+									speed: 500,
+									namespace: "callbacks",
+									before: function () {
+									  $('.events').append("<li>before event fired.</li>");
+									},
+									after: function () {
+									  $('.events').append("<li>after event fired.</li>");
+									}
+								  });
+							
+								}); */}
+						
+							
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+
+    );
+}
+export default About;
